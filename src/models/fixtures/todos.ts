0b121857884eb8cb6226a6b915bfee6770c72bc8
Nuno <ngocalves@gmail.com>
2021-03-11 20:32:52 +0100

    feat: Filter and Todo models

---
+++ src/models/fixtures/todos.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    title: "do my first todo",
+    done: false,
+    editing: false
+  },
+  {
+    id: 2,
+    title: "do my second todo",
+    done: false,
+    editing: false
+  }
+];
