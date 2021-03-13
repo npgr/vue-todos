63f38393778d944d041dbf104ff373401820f908
Nuno <ngocalves@gmail.com>
2021-03-13 22:37:41 +0100

    fix: remove trim() on filter by title

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,12 +1,12 @@
 import { Todo } from "@/models/Todo";
 import { TodoState } from "@/store";
 
-const plain = (title: string) => title.trim().toLowerCase();
+const plain = (title: string) => title.toLowerCase();
 
 export default {
   filteredTodos: ({ todos, filter }: TodoState): Todo[] => {
     const filteredByDone = todos.filter(({ done }) => done === filter.done);
-    return !filter.title.trim()
+    return !filter.title
       ? filteredByDone
       : filteredByDone.filter(({ title }) =>
           plain(title).includes(plain(filter.title))
